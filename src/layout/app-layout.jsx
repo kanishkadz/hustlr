d7b2ee876f2085de91f70e83e71074a4e1969922
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-12 14:45:35 +0530

    Update app-layout.jsx

--- src/layout/app-layout.jsx
+++ src/layout/app-layout.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
   return (
-    <div>AppLayout</div>
+    <div>AppLayout <Outlet /> </div>
   )
 }
 
