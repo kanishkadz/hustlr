25c5bd75ddcf177b5364db13cfd465acb065344a
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-12 22:06:36 +0530

    Update landing.jsx

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -22,7 +22,7 @@ const LandingPage = () => {
       </div>
 
       <Carousel className="w-full py-10" >
-        <CarouselContent>
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
           {companies.map(({name, id, path}) => {
             return <CarouselItem key={id} className="basis-1/3 lg:basis-1/6"> 
                   <img src={path} alt={name} className="h-9 sm:h-14 object-contain" /> 
