f26a0f57ca990335e666c1a8bfc7f6000af0e618
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-12 18:57:03 +0530

    header

--- src/layout/app-layout.jsx
+++ src/layout/app-layout.jsx
@@ -1,3 +1,4 @@
+import Header from '@/components/header'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
@@ -6,6 +7,7 @@ const AppLayout = () => {
     <div>
       <div className="grid-background"></div>
       <main className="min-h-screen container">
+        <Header />
         <Outlet /> 
       </main>
     </div>
