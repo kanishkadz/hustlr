363a653122b590def5355be10f63e8345b077bff
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-12 22:01:49 +0530

    Update landing.jsx

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import { CarouselContent, CarouselItem} from '@/components/ui/carousel'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -9,6 +10,7 @@ const LandingPage = () => {
         <h1 className="flex flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">Find Your Dream Job <span>and get hired</span></h1>
         <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">Explore thousands of job listings or find the perfect candidate.</p>
       </section>
+
       <div className="flex gap-6 justify-center">
         <Link to='/jobs'>
           <Button variant="blue" size="xl">Find Jobs</Button>
@@ -18,7 +20,11 @@ const LandingPage = () => {
         </Link>
       </div>
 
-      
+      <Carousel className="w-full py-10" >
+        <CarouselContent>
+          
+        </CarouselContent>
+      </Carousel>
     </main>
   )
 }
