b04611950d62836cd58a8fdb476d9c5bc86d679f
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-12 21:46:27 +0530

    Update landing.jsx

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -5,6 +5,7 @@ const LandingPage = () => {
     <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
       <section className="text-center">
         <h1 className="flex flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">Find Your Dream Job <span>and get hired</span></h1>
+        <p>Explore thousands of job listings or find the perfect candidate.</p>
       </section>
       <div>
 
