ebbedb2ac2ff9d148a2b8f6a894ae57b2dcb4fdf
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-12 14:51:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import AppLayout from "./layout/app-layout"
 import LandingPage from "./pages/landing"
 import Onboarding from "./pages/onboarding"
+import JobListing from "./pages/job-listing"
+import JobPage from "./pages/job"
+import PostJob from "./pages/post-job"
+import SaveJobs from "./pages/saved-job"
 
 const router = createBrowserRouter([
   {
@@ -14,6 +18,22 @@ const router = createBrowserRouter([
       {
         path: '/onboarding',
         element: <Onboarding />
+      },
+      {
+        path: '/jobs',
+        element: <JobListing />
+      },
+      {
+        path: '/job/:id',
+        element: <JobPage />
+      },
+      {
+        path: '/post-job',
+        element: <PostJob />
+      },
+      {
+        path: '/saved-job',
+        element: <SaveJobs />
       }
     ]
   }
