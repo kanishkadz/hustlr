1edd476020bb13a76b8423c9978ef6c70173c3a0
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-12 21:39:24 +0530

    Update landing.jsx

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LandingPage = () => {
   return (
     <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
-      <section>
+      <section className="text-center">
         <h1>Find Your Dream Job <span>and get hired</span></h1>
       </section>
       <div>
