4ed26969e94e720ae2fd97be5f9f618dbc2f8d25
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-13 12:23:39 +0530

    Update onboarding.jsx

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -11,7 +11,7 @@ const Onboarding = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center mt-40">
+    <div className="flex flex-col items-center justify-center mt-32">
       <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter">I am a... </h2>
       <div className="mt-16 grid grid-cols-2 gap-4 w-full md:px-40">
         <Button variant='blue' className='h-36 text-2xl'>Candidate</Button>
