0a70d2a5feaf55e5a489805c1f5796157deb50dc
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-13 15:33:49 +0530

    Update job-card.jsx

--- src/components/job-card.jsx
+++ src/components/job-card.jsx
@@ -1,10 +1,21 @@
+import { useUser } from '@clerk/clerk-react'
 import React from 'react'
+import { Card, CardHeader, CardTitle } from './ui/card';
 
-const JobCard = () => {
+const JobCard = ({
+    job, 
+    isMyJob = false, 
+    savedInit = false,
+    onJobSaved = () => {}, 
+}) => {
+    const {user} = useUser();
+    
   return (
-    <div>
-        
-    </div>
+    <Card>
+        <CardHeader>
+            <CardTitle>{job.title}</CardTitle>
+        </CardHeader>
+    </Card>
   )
 }
 
