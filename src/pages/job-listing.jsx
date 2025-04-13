487e2890b10f05d4c51af181bc834cc95446fe95
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-13 15:14:31 +0530

    Update job-listing.jsx

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,15 +1,21 @@
 import { getJobs } from '@/api/apiJobs'
 import useFetch from '@/hooks/use-fetch'
-import React, { useEffect } from 'react'
+import { useUser } from '@clerk/clerk-react'
+import React, { useEffect, useState } from 'react'
 
 const JobListing = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
+  const {isLoaded} = useUser()
+  
   const {fn: fnJobs, data:dataJobs, loading:loadingJobs} = useFetch(getJobs, {});
 
   console.log(dataJobs);
 
   useEffect(() => {
-    fnJobs();
-  }, [])
+    if (isLoaded) fnJobs();
+  }, [isLoaded]);
 
   return (
     <div>
