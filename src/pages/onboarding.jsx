b6ee25e8b9d4411b1865f53a2221f3e58f11e518
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-13 12:22:47 +0530

    Update onboarding.jsx

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import { useUser } from '@clerk/clerk-react'
 import React from 'react'
 import {BarLoader} from 'react-spinners'
@@ -12,6 +13,9 @@ const Onboarding = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-40">
       <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter">I am a... </h2>
+      <div className="mt-16 grid grid-cols-2 gap-4 w-full md:px-40">
+        <Button variant='blue' className='h-36 text-2xl'>Candidate</Button>
+      </div>
     </div>
   )
 }
