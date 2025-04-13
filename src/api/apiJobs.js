26279d375e4b4b454afe1ad87e24abe32aeee640
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-13 22:21:48 +0530

    Update apiJobs.js

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -50,3 +50,15 @@ export async function saveJob(token, {alreadySaved}, saveData){
         return data;
     }
 }
+
+export async function getSingleJob(token, {job_id}){
+    const supabase = await supabaseClient(token);
+
+    const {data, error} = await supabase.from("jobs").select("*, company: companies(name, logo, url), applications: applications(*)").eq("id", job_id).single();
+
+    if(error) {
+        console.error("Error Fetching Company:", error);
+        return null;
+    }
+    return data;    
+}
