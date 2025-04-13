678abb4775b8f86d4b475efe30b63fede1ec6970
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-13 12:30:12 +0530

    Update onboarding.jsx

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,10 +1,21 @@
 import { Button } from '@/components/ui/button';
 import { useUser } from '@clerk/clerk-react'
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import {BarLoader} from 'react-spinners'
 
 const Onboarding = () => {
   const {user, isLoaded} = useUser();
+
+  const navigate = useNavigate();
+
+  const handleRoleSelection = async(role) => {
+    await user.update({
+      unsafeMetadata: {role},
+    }).then(() => {
+      navigate(role === 'recruiter' ? "/post-job" : "/jobs");      
+    })
+  }
 
   if(!isLoaded){
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />
@@ -14,8 +25,8 @@ const Onboarding = () => {
     <div className="flex flex-col items-center justify-center mt-32">
       <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter">I am a... </h2>
       <div className="mt-16 grid grid-cols-2 gap-4 w-full md:px-40">
-        <Button variant='blue' className='h-36 text-2xl'>Candidate</Button>
-        <Button variant='destructive' className='h-36 text-2xl'>Recruiter</Button>
+        <Button variant='blue' className='h-36 text-2xl' onClick={() => handleRoleSelection("candidate")}>Candidate</Button>
+        <Button variant='destructive' className='h-36 text-2xl' onClick={() => handleRoleSelection("recruiter")}>Recruiter</Button>
       </div>
     </div>
   )
