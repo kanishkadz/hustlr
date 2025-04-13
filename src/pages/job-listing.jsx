2f592012b9a70865f7a6cfe79ecd6c2e0fe30f37
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-13 14:54:15 +0530

    Update job-listing.jsx

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { getJobs } from '@/api/apiJobs'
+import { useSession } from '@clerk/clerk-react'
+import React, { useEffect } from 'react'
 
 const JobListing = () => {
+  const {session} = useSession();
+  const fetchJobs = async () => {
+    const supabaseAccessToken = await session.getToken({
+      template: "supabase",
+    });
+    const data = await getJobs(supabaseAccessToken);
+  };
+
+  useEffect(() => {
+
+
+    getJobs();
+  }, [])
+
   return (
-    <div>JobListing</div>
+    <div>
+
+    </div>
   )
 }
 
