61d6dd9c0a5390de8edfd50d09f627be11a01fae
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-13 15:10:55 +0530

    Update apiJobs.js

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -5,6 +5,18 @@ export async function getJobs(token){
 
     let query = supabase.from("jobs").select("*");
 
+    if(location){
+        query = query.eq("location", location);
+    }
+
+    if(company_id){
+        query = query.eq("company_id", company_id);
+    }
+
+    if(searchQuery){
+        query = query.ilike("title", `%${searchQuery}`);
+    }
+
     const { data, error } = await query;
 
     if(error){
