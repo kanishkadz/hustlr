404e9b48cf3212c0a30d07feca6639bcdf55a497
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-13 22:24:14 +0530

    Update job.jsx

--- src/pages/job.jsx
+++ src/pages/job.jsx
@@ -1,6 +1,21 @@
+import { getSingleJob } from '@/api/apiJobs';
+import useFetch from '@/hooks/use-fetch';
+import { useUser } from '@clerk/clerk-react'
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const JobPage = () => {
+  const {isLoaded, user} = useUser();
+  const {id} = useParams();
+
+  const {
+    loading: loadingJob,
+    data: job,
+    fn: fnJob,
+  } = useFetch(getSingleJob, {
+    job_id: id,
+  });
+
   return (
     <div>JobPage</div>
   )
