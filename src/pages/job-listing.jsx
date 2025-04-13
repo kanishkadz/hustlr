67523031b80775fe2612141d8ea738f442d582a0
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-13 15:06:01 +0530

    Update job-listing.jsx

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,9 +1,15 @@
 import { getJobs } from '@/api/apiJobs'
 import useFetch from '@/hooks/use-fetch'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const JobListing = () => {
   const {fn: fnJobs, data:dataJobs, loading:loadingJobs} = useFetch(getJobs, {});
+
+  console.log(dataJobs);
+
+  useEffect(() => {
+    fnJobs();
+  }, [])
 
   return (
     <div>
