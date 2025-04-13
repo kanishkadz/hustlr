7952da24719e496df782f1e22fecd647b1577f38
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-13 12:19:12 +0530

    Update onboarding.jsx

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,8 +1,13 @@
 import { useUser } from '@clerk/clerk-react'
 import React from 'react'
+import {BarLoader} from 'react-spinners'
 
 const Onboarding = () => {
-  const {user} = useUser();
+  const {user, isLoaded} = useUser();
+
+  if(!isLoaded){
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />
+  }
 
   return (
     <div>Onboarding</div>
