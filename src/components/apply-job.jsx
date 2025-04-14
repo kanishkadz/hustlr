e1a92a6cdb68862ee63f2dfe746c2f9043eaca67
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-14 15:46:23 +0530

    Update apply-job.jsx

--- src/components/apply-job.jsx
+++ src/components/apply-job.jsx
@@ -65,7 +65,8 @@ const ApplyJobDrawer = ({ user, job, applied = false, fetchJob }) => {
 
                         {errors.education && (<p className="text-red-500">{errors.education.message}</p>)}
 
-                        <Input type="file" accept=".pdf .doc .docx" className="flex-1 file:text-gray-500" />
+                        <Input type="file" accept=".pdf .doc .docx" className="flex-1 file:text-gray-500" {...register("resume")} />
+                        {errors.resume && (<p className="text-red-500">{errors.resumeme.message}</p>)}
                         <Button type="submit" variant="blue" size="lg">Apply</Button>
 
                     </form>
