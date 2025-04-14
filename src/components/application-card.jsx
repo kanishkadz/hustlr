3c77b733fc96aa922a1d370a158249faeb791792
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-14 16:19:17 +0530

    Update application-card.jsx

--- src/components/application-card.jsx
+++ src/components/application-card.jsx
@@ -3,12 +3,19 @@ import { Card, CardHeader, CardTitle } from './ui/card'
 import { Download } from 'lucide-react'
 
 const ApplicationCard = ({ application, isCandidate = false }) => {
+    const handleDownload () => {
+        const link = document.createElement("a");
+        link.href = application?.resume;
+        link.target = "_blank";
+        link.click();
+    }
+
   return (
     <Card>
         <CardHeader>
             <CardTitle className="flex justify-between font-bold">
                 {isCandidate ? `${application?.job?.title} at ${application?.job?.company?.name}` : application?.name}
-                <Download size={18} className="bg-white text-black rounded-full h-8 w-8 p-1.5 cursor-pointer" />
+                <Download size={18} className="bg-white text-black rounded-full h-8 w-8 p-1.5 cursor-pointer" onClick={handleDownload} />
             </CardTitle>
         </CardHeader>
     </Card>
