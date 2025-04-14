9d172315316e9a41834a10fed033c3d4c393e773
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-14 12:47:05 +0530

    Update apply-job.jsx

--- src/components/apply-job.jsx
+++ src/components/apply-job.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from './ui/drawer'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+
 
 const ApplyJobDrawer = ({ user, job, applied = false, fetchJob }) => {
     return (
         <div>
             <Drawer open={applied ? false : undefined}>
-                <DrawerTrigger asChild> 
+                <DrawerTrigger asChild>
                     <Button size="lg" variant={job?.isOpen && !applied ? "blue" : "destructive"} disabled={!job?.isOpen || applied}>
                         {job?.isOpen ? (applied ? "Applied" : "Apply") : "Hiring Closed"}
                     </Button>
@@ -21,6 +24,18 @@ const ApplyJobDrawer = ({ user, job, applied = false, fetchJob }) => {
                     <form className="flex flex-col gap-4 p-4 pb-0">
                         <Input type="number" placeholder="Years of Experience" className="flex-1" />
                         <Input type="text" placeholder="Skills (Comma Seperated)" className="flex-1" />
+
+                        <RadioGroup defaultValue="option-one">
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="option-one" id="option-one" />
+                                <Label htmlFor="option-one">Option One</Label>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="option-two" id="option-two" />
+                                <Label htmlFor="option-two">Option Two</Label>
+                            </div>
+                        </RadioGroup>
+
                     </form>
 
                     <DrawerFooter>
