6a885e5e224e714cd6a3d92292c7948edfab7a42
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-14 12:37:41 +0530

    Update apply-job.jsx

--- src/components/apply-job.jsx
+++ src/components/apply-job.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Drawer, DrawerClose, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from './ui/drawer'
+import { Button } from './ui/button'
 
-const ApplyJobDrawer = () => {
-  return (
-    <div>ApplyJobDrawer</div>
-  )
+const ApplyJobDrawer = ({ user, job, applied = false, fetchJob }) => {
+    return (
+        <div>
+            <Drawer>
+                <DrawerTrigger>Open</DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                        <DrawerDescription>This action cannot be undone.</DrawerDescription>
+                    </DrawerHeader>
+                    <DrawerFooter>
+                        <Button>Submit</Button>
+                        <DrawerClose>
+                            <Button variant="outline">Cancel</Button>
+                        </DrawerClose>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+
+        </div>
+    )
 }
 
 export default ApplyJobDrawer
