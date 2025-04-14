60b91b90c6e75bd9f1e6ce98b5d19cdf29072c15
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-14 12:40:03 +0530

    Update apply-job.jsx

--- src/components/apply-job.jsx
+++ src/components/apply-job.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Drawer, DrawerClose, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from './ui/drawer'
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from './ui/drawer'
 import { Button } from './ui/button'
 
 const ApplyJobDrawer = ({ user, job, applied = false, fetchJob }) => {
     return (
         <div>
-            <Drawer>
-                <DrawerTrigger>Open</DrawerTrigger>
+            <Drawer open={applied ? false : undefined}>
+                <DrawerTrigger>
+                    <Button>
+                        {job?.isOpen ? (applied ? "Applied" : "Apply") : "Hiring Closed"}
+                    </Button>
+                </DrawerTrigger>
                 <DrawerContent>
                     <DrawerHeader>
                         <DrawerTitle>Are you absolutely sure?</DrawerTitle>
