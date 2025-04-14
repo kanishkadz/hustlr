3752d6b7b8e27296cd6b9e269f34ba4ff128f6e7
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-14 16:15:42 +0530

    Update application-card.jsx

--- src/components/application-card.jsx
+++ src/components/application-card.jsx
@@ -6,7 +6,7 @@ const ApplicationCard = ({ application, isCandidate = false }) => {
   return (
     <Card>
         <CardHeader>
-            <CardTitle>
+            <CardTitle className="flex justify-between font-bold">
                 {isCandidate ? `${application?.job?.title} at ${application?.job?.company?.name}` : application?.name}
                 <Download size={18} className="bg-white text-black rounded-full h-8 w-8 p-1.5 cursor-pointer" />
             </CardTitle>
