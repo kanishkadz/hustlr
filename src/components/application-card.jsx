0181fe2fc3b29cc338ea1fc9663858e83124f317
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-14 16:23:58 +0530

    Update application-card.jsx

--- src/components/application-card.jsx
+++ src/components/application-card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
-import { BriefcaseBusiness, Download } from 'lucide-react'
+import { Boxes, BriefcaseBusiness, Download, School } from 'lucide-react'
 
 const ApplicationCard = ({ application, isCandidate = false }) => {
     const handleDownload = () => {
@@ -20,8 +20,10 @@ const ApplicationCard = ({ application, isCandidate = false }) => {
         </CardHeader>
 
         <CardContent>
-            <div>
+            <div className="flex flex-col md:flex-row justify-between">
                 <div className="flex gap-2 items-center"><BriefcaseBusiness size={15} /> {application?.experience} years of experience.</div>
+                <div className="flex gap-2 items-center"><School size={15} /> {application?.education}</div>
+                <div className="flex gap-2 items-center"><Boxes size={15} /> Skills: {application?.skills}</div>
             </div>
         </CardContent>
     </Card>
