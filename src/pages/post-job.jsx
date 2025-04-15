dbf1ec0a715047203604e880274cbebcb9a91855
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-15 18:52:29 +0530

    Update post-job.jsx

--- src/pages/post-job.jsx
+++ src/pages/post-job.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { z } from 'zod'
+
+const schema = z.object({
+  title: z.string().min(1, {message: "Title is reequest"}),
+  description: z.string().min(1, {message: "Description is required"}),
+  location: z.string().min(1, {message: "Select a location"}),
+  company_id: z.string().min(1, {message: "Select or Add a new Company"}),
+  requirements: z.string().min(1, {message: "Requirements are required"}),
+});
 
 const PostJob = () => {
+
+
   return (
     <div>PostJob</div>
   )
