67d4b42d9b4a9404f3940051e97438ffe461f861
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-16 12:18:30 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
     <>
       <nav className="py-4 flex justify-between items-center">
         <Link to="/">
-          <img src="/logo.png" className="h-20" alt="Hirrd Logo" />
+          <img src="/logo.png" className="h-35" alt="HustLr Logo" />
         </Link>
 
         <div className="flex gap-8">
